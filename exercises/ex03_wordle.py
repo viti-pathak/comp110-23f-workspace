3f620531c71e56de73506633ab6a39303139af7d
Viti Pathak <vpathak@unc.edu>
2023-10-02 20:30:39 -0400

    Progress on Exercise 03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,21 +1,19 @@
-"""EX03 - Structured Wordle"""
+"""EX03 - Structured Wordle."""
 __author__ = "730523434"
 
-def contains_char(word: str, character: str) -> bool: 
-    """checks to see if character is contained in guess"""
+def contains_char(word: str, character: str) -> bool:
+    """Checks if a character is contained in a word."""
     assert len(character) == 1
     position: bool = False
     index: int = 0
-    while position is False and index < len(word):
+    while not position and index < len(word):
         if word[index] == character:
             position = True
-        else:
-            position = False
-        index = index + 1
+        index += 1
     return position
 
 def emojified(guess: str, secret: str) -> str:
-    """prints out colored emoji boxes"""
+    """Prints out colored emoji boxes."""
     assert len(guess) == len(secret)
 
     i: int = 0
@@ -27,19 +25,20 @@ def emojified(guess: str, secret: str) -> str:
 
     while i < len(secret):
         if guess[i] == secret[i]:
-            emoji = emoji + GREEN_BOX
+            emoji += GREEN_BOX
         else:
-            if contains_char(secret, guess[i]) == True:
-                    emoji = emoji + YELLOW_BOX
+            if contains_char(secret, guess[i]):
+                emoji += YELLOW_BOX
             else:
-                    emoji = emoji + WHITE_BOX
-        i = i + 1
+                emoji += WHITE_BOX
+        i += 1
     return emoji
 
 def input_guess(length: int) -> str:
-    guess: str = input(f"Enter a { length } character word: ")
+    """Gets user input for a word guess."""
+    guess: str = input(f"Enter a {length} character word: ")
     while len(guess) != length:
-        guess = input(f"That wasn't { length } chars! Try again: ")
+        guess = input(f"That wasn't {length} characters! Try again: ")
     return guess
 
 def main() -> None:
@@ -47,16 +46,16 @@ def main() -> None:
     secret: str = "codes"
     turns: int = 1
     won: bool = False
-    while turns < 7 and won is False:
-        print(f"=== Turn { turns }/6 ===")
+    while turns < 7 and not won:
+        print(f"=== Turn {turns}/6 ===")
         guess: str = input_guess(len(secret))
         print(emojified(guess, secret))
         if guess == secret:
             won = True
         else:
-            turns = turns + 1
-    if won is True:
-        print(f"You won in { turns }/6 turns!")
+            turns += 1
+    if won:
+        print(f"You won in {turns}/6 turns!")
     else:
         print("X/6 - Sorry, try again tomorrow!")
 

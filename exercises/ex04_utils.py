d5561fe451dafc87b39aeea2526505af58448ec7
Viti Pathak <vpathak@unc.edu>
2023-10-11 15:48:40 -0400

    progress of ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,36 +1,33 @@
 """EX04 - 'list' Utility Functions."""
 __author__ = "730523434"
 
+
 def all(xs: list[int], a: int) -> bool:
-    """Returns True if all numbers in the list match the given number, False otherwise or if the list is empty."""
+    """Returns True if all numbers in the list match the given number, False otherwise."""
     if len(xs) == 0:
         return False
-    
     for x in xs:
         if x != a:
             return False
-    
     return True
 
+
 def max(input: list[int]) -> int:
-    """Returns the largest number in the list."""
+    """Returns the largest number in the list. If the list is empty, raises a ValueError."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-
-    largest = input[0]  # Initialize largest with the first element
+    largest = input[0]
     for num in input:
         if num > largest:
             largest = num
-
     return largest
 
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """Returns True if each number at each index is equal in both lists and if not, returns False."""
+    """Returns True if each number at each index is equal in both lists, False otherwise."""
     if len(list1) != len(list2):
         return False
-
     for i in range(len(list1)):
         if list1[i] != list2[i]:
             return False
-
     return True
